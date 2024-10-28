602264dc8e58706154c58065943559d499788633
Sara-ibrahiim <sou.cute94@gmail.com>
2024-10-28 15:11:27 +0300

    Forget

--- src/Pages/AuthPages/ForgetPass/ForgetPassword.tsx
+++ src/Pages/AuthPages/ForgetPass/ForgetPassword.tsx
@@ -1,5 +1,16 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 export default function ForgetPassword() {
-  return <div>ForgetPassword</div>;
+
+
+  return <>
+  <Box sx={{w:"100%", display:"flex",flexDirection:"column"}}>
+
+
+
+  </Box>
+  
+  
+  </>;
 }
