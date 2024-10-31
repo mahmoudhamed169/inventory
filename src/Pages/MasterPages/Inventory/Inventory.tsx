11a3b36c166a648f8919802cc12776e4bb6ba170
mahmoudhamed169 <mh4190207@gmail.com>
2024-10-31 19:20:37 +0300

    add product modal ui

--- src/Pages/MasterPages/Inventory/Inventory.tsx
+++ src/Pages/MasterPages/Inventory/Inventory.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import AddProductModal from "../../../Components/MasterComponnets/AddProductModal/AddProductModal";
 
 export default function Inventory() {
-  return <div>Inventory</div>;
+  return <div></div>;
 }
