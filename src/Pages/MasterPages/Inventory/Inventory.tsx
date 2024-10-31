546b4e5c327fcff8eff7894786869873ed3c759a
mahmoudhamed169 <mh4190207@gmail.com>
2024-10-31 19:36:39 +0300

    add product

--- src/Pages/MasterPages/Inventory/Inventory.tsx
+++ src/Pages/MasterPages/Inventory/Inventory.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import AddProductModal from "../../../Components/MasterComponnets/AddProductModal/AddProductModal";
 
 export default function Inventory() {
-  return (
-    <div>
-      <AddProductModal />
-    </div>
-  );
+  return <div>{/* <AddProductModal /> */}</div>;
 }
