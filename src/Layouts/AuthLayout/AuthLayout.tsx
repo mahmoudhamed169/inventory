ab2814cd78769bc9004d200ae3c39dbd70e2bd01
mahmoudhamed169 <mh4190207@gmail.com>
2024-11-01 14:46:22 +0200

    Loading page eith loading context

--- src/Layouts/AuthLayout/AuthLayout.tsx
+++ src/Layouts/AuthLayout/AuthLayout.tsx
@@ -2,9 +2,27 @@ import { Box, Grid } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import AuthPageTitle from "../../Utils/AuthPageTitle";
 import AuthBackground from "../../Components/AuthComponents/AuthBackGround/AuthBackGround";
+import { useEffect, useState } from "react";
+import MainLoading from "../../Components/MainLoading/MainLoading";
+import { useLoading } from "../../Context/LoadingContext/LoadingContext";
 
 export default function AuthLayout() {
+  const { loading, setLoading } = useLoading();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timer);
+      setLoading(true);
+    };
+  }, [setLoading]);
   AuthPageTitle();
+
+  if (loading) {
+    return <MainLoading />;
+  }
 
   return (
     <Grid container spacing={3} sx={{ height: { xs: "auto", md: "100vh" } }}>
