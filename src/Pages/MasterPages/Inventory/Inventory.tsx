3daadad1ce9f4b1bd7fe34564b5197af7c039a23
Sara-ibrahiim <sou.cute94@gmail.com>
2024-11-05 19:37:58 +0200

    Integration

--- src/Pages/MasterPages/Inventory/Inventory.tsx
+++ src/Pages/MasterPages/Inventory/Inventory.tsx
@@ -1,4 +1,12 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  Grid2,
+  InputLabel,
+  Select,
+  Stack,
+  TextField,
+} from "@mui/material";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -12,39 +20,66 @@ import Pagination from "../../../Components/MasterComponnets/Pagination/Paginati
 import { HeaderTable } from "../../../Components/MasterComponnets/InventoryAndOrders/Text";
 import { apiClient, PRODUCTS_URLS } from "../../../Api/EndPoints";
 import { useEffect, useState } from "react";
+import MenuItem from "@mui/material/MenuItem";
 
 export default function Inventory() {
-  const rows = Array.from({ length: 9 }, (_, index) => ({
-    Products: `Product ${index + 1}`,
-    Price: `₹ ${(index + 1) * 10}`,
-    Quantity: 30 - index,
-    ThresholdValue: 12 - index,
-    ExpiryDate: "21/12/22",
-    Availability: "inStock",
-  }));
-
   const cellStyle = {
     color: "#667085",
     fontWeight: 500,
     fontFamily: "Inter, serif",
   };
-const [productList, setProductList] = useState([])
-
-  let getAllProduct = async ()=> {
+  const [productList, setProductList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pageSize] = useState(9);
+  const [totalPages, setTotalPages] = useState(0);
+  const [nameValue, setNameValue] = useState("");
+  const [availabilityValue, setAvailabilityValue] = useState();
+  let getAllProduct = async (
+    pageNumber: number,
+    pageSize: number,
+    nameInput: string,
+    available?: number | string
+  ) => {
     try {
-      let response = await apiClient.get(PRODUCTS_URLS.GetAllProducts)
-      console.log(response.data.data)
-      
+      let response = await apiClient.get(PRODUCTS_URLS.GetAllProducts, {
+        params: {
+          PageNumbar: pageNumber,
+          PageSize: pageSize,
+          Name: nameInput,
+          Available: available,
+        },
+      });
+      console.log(response.data);
+
+      setProductList(response.data.data);
+      // setTotalPages(Math.ceil(response.data. / pageSize));
     } catch (error) {
-      
-
+      console.log(error)
     }
-  }
+  };
 
+  // useEffect(() => {
+  //   getAllProduct(pageSize, page);
+  // }, [page, pageSize]);
   useEffect(() => {
-    getAllProduct()
-  }, [])
-  
+    getAllProduct(1, 9, "");
+  }, []);
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
+  const getNameValue = (input: any) => {
+    setNameValue(input.target.value);
+    getAllProduct(1, 9, input.target.value, availabilityValue);
+  };
+  const getAvailabilityValue = (input: any) => {
+    setAvailabilityValue(input.target.value);
+    getAllProduct(1, 9, nameValue, input.target.value);
+  };
+
   return (
     <>
       <Stack spacing={3} sx={{ py: 3, ps: "16px", mx: 2 }}>
@@ -58,80 +93,198 @@ const [productList, setProductList] = useState([])
         >
           <Box
             sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              my: 4,
+              my: 3,
               paddingInline: "16px",
             }}
           >
-          <HeaderTable headerTable="Products" /> 
-            <AddProductModal />
+            <Grid2 container>
+              <Grid2 size={{ xs: 12, md: 1 }}>
+                <Box sx={{ mt: 1, py: { xs: 2, md: 0 } }}>
+                  <HeaderTable headerTable="Products" />
+                </Box>
+              </Grid2>
+              <Grid2 size={{ xs: 12, md: 5 }}>
+                <Box
+                  sx={{
+                    "& .MuiTextField-root": { width: "45ch" },
+                    display: "flex",
+                    justifyContent: { xs: "flex-start", md: "center" },
+                  }}
+                >
+                  <Box>
+                    <TextField
+                      id="outlined-size-small"
+                      sx={{
+                        ".css-16wblaj-MuiInputBase-input-MuiOutlinedInput-input":
+                          {
+                            padding: "14px",
+                          },
+                        bgcolor: "#f5f6f8",
+                      }}
+                      type="text"
+                      placeholder="Search by Name ..."
+                      onChange={getNameValue}
+                         label="Search by Name"
+                    />
+                  </Box>
+                </Box>
+              </Grid2>
+              <Grid2 size={{ xs: 12, md: 1 }}>
+                <Box
+                  sx={{ minWidth: 120, maxWidth: 360, my: { xs: 2, md: 0 } }}
+                >
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">
+                      Filters
+                    </InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      label="Filters"
+                      onChange={getAvailabilityValue}
+                      sx={{
+                        ".css-w76bbz-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
+                          { padding: "11px  14px 16px 14px" },
+                      }}
+                    >
+                      <MenuItem value={0}>In Stock</MenuItem>
+                      <MenuItem value={1}>Out Of Stock</MenuItem>
+                      <MenuItem value={2}>Low Stock</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+              </Grid2>
 
-            {/* <Button
-              type="submit"
-              sx={{
-                backgroundColor: "#1366D9",
-                textTransform: "none",
-                fontSize: "14px",
-                fontWeight: 500,
-                gap: "8px",
-                fontFamily: "Inter, serif",
-              }}
-              variant="contained"
-            >
-              Add Product
-            </Button> */}
+              <Grid2 size={{ xs: 12, md: 5 }}>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: { xs: "flex-start", md: "flex-end" },
+                  }}
+                >
+                  <AddProductModal />
+                </Box>
+              </Grid2>
+            </Grid2>
           </Box>
+
           <TableContainer component={"table"}>
             <Table sx={{ minWidth: 660 }} aria-label="simple table">
               <TableHead>
                 <TableRow>
-                  <TableCell sx={cellStyle}>Products</TableCell>
-                  <TableCell sx={cellStyle} align="left">
+                  <TableCell
+                    sx={{ ...cellStyle, fontSize: { xs: "12px", md: "14px" } }}
+                  >
+                    Products
+                  </TableCell>
+                  <TableCell
+                    sx={{ ...cellStyle, fontSize: { xs: "12px", md: "14px" } }}
+                    align="left"
+                  >
                     Buying Price
                   </TableCell>
-                  <TableCell sx={cellStyle} align="left">
+                  <TableCell
+                    sx={{ ...cellStyle, fontSize: { xs: "12px", md: "14px" } }}
+                    align="left"
+                  >
                     Quantity
                   </TableCell>
-                  <TableCell sx={cellStyle} align="left">
+                  <TableCell
+                    sx={{ ...cellStyle, fontSize: { xs: "12px", md: "14px" } }}
+                    align="left"
+                  >
                     Threshold Value
                   </TableCell>
-                  <TableCell sx={cellStyle} align="left">
+                  <TableCell
+                    sx={{ ...cellStyle, fontSize: { xs: "12px", md: "14px" } }}
+                    align="left"
+                  >
                     Expiry Date
                   </TableCell>
-                  <TableCell sx={cellStyle} align="left">
+                  <TableCell
+                    sx={{ ...cellStyle, fontSize: { xs: "12px", md: "14px" } }}
+                    align="left"
+                  >
                     Availability
                   </TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {rows.map((row) => (
-                  <TableRow
-                    key={row.Products}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      <Link
-                        to={"/home/profuct-info"}
-                        style={{
-                          textDecoration: "none",
-                          color: "#48505E",
+                {productList && productList.length > 0
+                  ? productList.map((row: any) => (
+                      <TableRow
+                        key={row?.id}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                     
                         }}
                       >
-                        {row.Products}
-                      </Link>
-                    </TableCell>
-                    <TableCell align="left">{row.Price}</TableCell>
-                    <TableCell align="left">{row.Quantity} Packets</TableCell>
-                    <TableCell align="left">{row.ThresholdValue}</TableCell>
-                    <TableCell align="left">{row.ExpiryDate}</TableCell>
-                    <TableCell align="left">{row.Availability}</TableCell>
-                  </TableRow>
-                ))}
+                        <TableCell
+                          component="th"
+                          scope="row"
+                          sx={{ fontSize: { xs: "12px", md: "14px" } }}
+                        >
+                          <Link
+                            to={"/home/profuct-info"}
+                            style={{
+                              textDecoration: "none",
+                              color: "#48505E",
+                            }}
+                          >
+                            {row?.name}
+                          </Link>
+                        </TableCell>
+                        <TableCell
+                          sx={{ fontSize: { xs: "12px", md: "14px" } ,color: "#48505E" }}
+                          align="left"
+                        >₹
+                          {row?.price}
+                        </TableCell>
+                        <TableCell
+                          sx={{ fontSize: { xs: "12px", md: "14px" } ,color: "#48505E" }}
+                          align="left"
+                        >
+                          {row?.quantity} Packets
+                        </TableCell>
+                        <TableCell
+                          sx={{ fontSize: { xs: "12px", md: "14px" } ,color: "#48505E" }}
+                          align="left"
+                        >
+                          {row?.threshold}
+                        </TableCell>
+                        <TableCell
+                          sx={{ fontSize: { xs: "12px", md: "14px" } ,color: "#48505E" }}
+                          align="left"
+                        >
+                          {new Date(row?.expiryDate).toLocaleDateString()}
+                        </TableCell>
+                        <TableCell
+                          sx={{
+                            color:
+                              row?.available === "InStock"
+                                ? "#10A760"
+                                : row?.available === "OutOfStock"
+                                ? "#DA3E33"
+                                : row?.available === "LowStock"
+                                ? "#E19133"
+                                : "inherit",
+                            fontSize: { xs: "12px", md: "14px" } ,
+                          }}
+                          align="left"
+                        >
+                          {row?.available}
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  : ""}
               </TableBody>
             </Table>
           </TableContainer>
-          <Pagination />
+          <Pagination
+          // count={totalPages}
+          // page={page}
+          // onChange={handlePageChange}
+          />
         </Box>
       </Stack>
     </>
